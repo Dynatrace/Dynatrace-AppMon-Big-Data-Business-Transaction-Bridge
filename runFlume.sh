69b5c24d401321a9cba605b3ab5ff934b1063217
ingohackl <ingo.hackl@dynatrace.com>
2014-12-23 08:48:46 +0100

    version 1.0.1

--- runFlume.sh
+++
@@ -1,5 +0,0 @@
-
-FLUME_HOME=./apache-flume-1.3.1-bin
-FLUME_CONF=flume-conf.properties
-mkdir `grep directory $FLUME_CONF | sed "s#.*directory = \(.*\)#\1#g"`
-$FLUME_HOME/bin/flume-ng agent -C dtFlume.jar -n agent1 -c $FLUME_HOME/conf -f $FLUME_CONF -Dflume.root.logger=INFO,console
